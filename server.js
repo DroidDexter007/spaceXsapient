fd704853e5054311e3ff3637b619c78342c972d6
Subhash Prakash <subpraka@cisco.com>
2020-09-09 13:32:34 +0530

    heroku second deployment

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/space-x-sapient'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/space-x-sapient/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/space-x-sapient/index.html'));});
 app.listen(process.env.PORT || 8080);
