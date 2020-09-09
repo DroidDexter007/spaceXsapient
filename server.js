9b8590200ad994c00a2e01918eecc9e37de716e3
Subhash Prakash <subpraka@cisco.com>
2020-09-09 14:46:22 +0530

    package json changed

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/space-x-sapient'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/dist/space-x-sapient/index.html'));});
+    
+res.sendFile(path.join(__dirname,'/dist/space-x-sapient/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static('./dist/space-x-sapient'));
+// app.get('/*', function(req,res) {
+// res.sendFile(__dirname,'/dist/space-x-sapient/index.html');});
+// app.listen(process.env.PORT || 8080);
