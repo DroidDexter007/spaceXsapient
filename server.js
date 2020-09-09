a73caf279706ac1dca860283334b04ce2f3713d8
Subhash Prakash <subpraka@cisco.com>
2020-09-09 14:04:19 +0530

    server js changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/space-x-sapient'));
+app.use(express.static('./dist/space-x-sapient'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/space-x-sapient/index.html'));});
+res.sendFile(path.join(__dirname,'/dist/space-x-sapient/index.html'));});
 app.listen(process.env.PORT || 8080);
